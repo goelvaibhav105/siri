fa2bbed912de2892d62ddcf71efe7b935384203f
Vaibhav goel <goelvaibhav105@gmail.com>
2024-06-17 16:34:30 +0530

    Basic image added designe9ng lower

--- components/Recorder.tsx
+++ components/Recorder.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import activeAssistantIcon from '../img/active.gif'
+import notActiveAssistantIcon from '../img/nontactive.gif'
+import Image from 'next/image'
+
 
 function Recorder() {
   return (
-    <div className='flex items-center justify-center'>Recorder</div>
+    <div className='flex items-center justify-center'>
+      <Image
+      src={activeAssistantIcon}
+      width={150}
+      height={150}
+      alt='Recording'
+      priority
+
+      />
+    
+    </div>
   )
 }
 
